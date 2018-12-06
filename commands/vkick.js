d9610e09662536276bf74cfba41b9017f18effb4
Poeticalto <poeticaltotp@gmail.com>
2018-12-06 00:50:31 -0600

    fix permissions issue

--- commands/vkick.js
+++ commands/vkick.js
@@ -1,24 +1,24 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-    if (!message.guild.me.hasPermission(['MANAGE_CHANNELS', 'MOVE_MEMBERS'])) return message.reply('Sorry, missing permissions.');
-
-    const member = message.mentions.members.first();
+   const member = message.mentions.members.first();
     if (!member) return message.reply('Need to @mention a user/bot to voice kick.');
     if (!member.voiceChannel) return message.reply('That user/bot isn\'t in a voice channel.');
 
-    const temp_channel = await message.guild.createChannel(user.id, 'voice', [
-        {id: guild.id,
+    const temp_channel = await message.guild.createChannel(member.id, 'voice', [
+        {id: message.guild.id,
          deny: ['VIEW_CHANNEL', 'CONNECT', 'SPEAK'], },
         {id: member.id,
          deny: ['VIEW_CHANNEL', 'CONNECT', 'SPEAK'] }
     ]);
+	await member.setVoiceChannel(temp_channel);
+	console.log("here");
     await temp_channel.delete();
-
+	console.log("test");
     message.reply('User successfully kicked!');
 };
 
 exports.conf = {
     enabled: true,
-    guildOnly: false,
+    guildOnly: true,
     aliases: [],
     permLevel: "Bot Admin"
 };
