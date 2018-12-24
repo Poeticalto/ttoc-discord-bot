0d76ab903d8dd9cf81f98f8a7dcb0017ba8c4e62
Poeticalto <poeticaltotp@gmail.com>
2018-12-24 00:42:02 -0600

    prevent message loop

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -7,7 +7,7 @@ module.exports = (client, message) => {
 	const args = message.content.slice(client.getSettings(message.guild.id).prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 	const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
-	if (!cmd && message.author.bot === false) {
+	if (!cmd && message.author.bot === false && message.channel.name !== client.config.trashChannel) {
 		message.guild.channels.find(channel => channel.name === client.config.trashChannel).send(`Message from ${message.member.displayName} (${message.member.id}) deleted:\n ${message.cleanContent}`).catch(client.logger.log("no trashChannel found"));
 	}
 }
