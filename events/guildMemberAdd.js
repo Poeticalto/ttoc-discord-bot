eb906e8c2df79dd9808c77d9a3c5ebe2422a65e0
Anthony Nguyen <poeticaltotp@gmail.com>
2019-02-22 21:24:38 -0600

    edit new member message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,7 +4,6 @@
 
 module.exports = async (client, member) => {
     // find the channel, then send the welcome message
-    
     let someBallRole = member.guild.roles.find(role => role.name === "Some Ball");
     member.addRole(someBallRole).catch(console.error);
     let currentTime = new Date().getTime();
@@ -24,5 +23,5 @@ module.exports = async (client, member) => {
         convertedDifference = (differenceTime / 86400000) + " days";
     }
     member.guild.channels.find(channel => channel.name === "join-feed").send(`Age of ${member} is: ${convertedDifference}`);
-    member.guild.channels.find(channel => channel.name === client.config.welcomeChannel).send(`Welcome to the ${member.guild.name} Discord, ${member}!\nYou need to be verified by an admin or a captain before you are allowed to roam freely. If you can't reach an admin here, send a private message to your captain asking them to give you a team role!`);
+    member.guild.channels.find(channel => channel.name === client.config.welcomeChannel).send(`Welcome to the ${member.guild.name} Discord, ${member}!\nYou need to be verified by an admin or a captain before you are allowed to chat in general channels. If an admin isn't available, send a private message to your captain asking them to give you a team role!`);
 };
